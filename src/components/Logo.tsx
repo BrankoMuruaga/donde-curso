e5aaae1e918b181ca9b5881c2d98bcec2c93273a
Branko Muruaga <muruagabranko253@gmail.com>
2025-01-28 23:55:03 -0300

    Update Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 function Logo() {
   return (
-    <a className="items-center max-h-14 ml-7" href="/">
+    <Link className="items-center max-h-14 ml-7" href="/">
       <img
         className="h-14 w-auto"
         src="/UNAHUR-LOGO.webp"
         alt="Logo Universidad Nacional de Hurlingham"
       />
-    </a>
+    </Link>
   );
 }
 
